0cfed1204def5b54eb6f642e71ada244e4f4ed5a
Semen95 <semaforever1995@gmail.com>
2022-08-28 14:21:36 +0300

    Add 44

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,10 @@ function App() {
             <div> Dev branch commit 3</div>
             <div> develop/login branch commit 1</div>
             <div> develop/login branch commit 2</div>
+            <p>11</p>
+            <p>22</p>
+            <p>33</p>
+            <p>44</p>
         </div>
     );
 }
