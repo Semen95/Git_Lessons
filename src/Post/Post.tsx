2cfc240ca9f5476d24a7f5108761224083e0d21a
Semen95 <semaforever1995@gmail.com>
2022-08-28 14:30:48 +0300

    post

---
+++ src/Post/Post.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const Post = () => {
+    const sum=(a:number, b:number)=>{
+        return a+b;
+    }
+    return (
+        <div>
+            <h2>Post</h2>
+            <button onClick={()=>sum(2,9)}>sum</button>
+        </div>
+    );
+};
+
