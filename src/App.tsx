00d28004e92f12159f545849d74040e20107908b
Semen95 <semaforever1995@gmail.com>
2022-08-28 13:44:46 +0300

    develop/popup branch commit 1

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,11 @@ import './App.css';
 
 function App() {
     return (<div>
-            <div>
-                App
-            </div>
-            <div>
-                Dev branch commit 1
-            </div>
-            <div>
-                Dev branch commit 2
-            </div>
-            <div>
-                Dev branch commit 3
-            </div>
+            <div> App</div>
+            <div> Dev branch commit 1</div>
+            <div> Dev branch commit 2</div>
+            <div> Dev branch commit 3</div>
+            <div> develop/popup branch commit 1</div>
         </div>
     );
 }
